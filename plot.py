6df8914b33875037e24cf4209a4418bd931b4131
Tobias Weis <weis@ccc.cs.uni-frankfurt.de>
2019-08-04 11:20:47 +0200

    adapted preprocessing to larger data source

--- plot.py
+++ plot.py
@@ -18,9 +18,25 @@ sensorvalues = db.get_all()
 sensors = db.get_sensors()
 
 measurements = {}
+print sensors
+ids = []
+for sensor in sensors:
+    ids.append(sensor.uniqueid)
 
 daynames = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
 
+# no more expensive dicts, too much for the rpi
+myvals = np.zeros((7,24,len(ids)), np.float64)
+for sv in sensorvalues:
+    if sv.motion:
+        myvals[sv.dtime.weekday(),sv.dtime.hour,ids.index(sv.idsensor)] += 1.
+
+for day in range(myvals.shape[0]):
+    for sensor in range(len(ids)):
+        myvals[day,:,sensor] /= np.sum(myvals[day,:,sensor])
+
+       
+"""
 for sv in sensorvalues:
     if sv.idsensor not in measurements.keys():
         measurements[sv.idsensor] = {}
@@ -34,34 +50,30 @@ for sv in sensorvalues:
     measurements[sv.idsensor]["dtime"].append(sv.dtime)
     measurements[sv.idsensor]["hours"].append(sv.dtime.hour)
     measurements[sv.idsensor]["weekdays"].append(sv.dtime.weekday())
-    measurements[sv.idsensor]["temp"].append(sv.temperature)
-    measurements[sv.idsensor]["light"].append(sv.lightlevel)
+    
+    #measurements[sv.idsensor]["temp"].append(sv.temperature)
+    #measurements[sv.idsensor]["light"].append(sv.lightlevel)
     measurements[sv.idsensor]["motion"].append(sv.motion)
+"""
+sensovalues=None
+print("all sensorvalues read from db")
 
 ##### MOTION HEATMAP PER DAY AND HOUR
 # create an array that holds the weekday in the row, the hour in the column,
 # and the count of motion-events as value
-for idsensor in measurements.keys():
+for idsensor in ids:
     fig = plt.figure()
     for s in sensors:
         if idsensor == s.uniqueid:
             name = s.name
     plt.suptitle(name)
 
-    myvals = np.zeros((7,24), np.float64)
-    motion_arr = np.array(measurements[idsensor]["motion"])
-    day_arr = np.array(measurements[idsensor]["weekdays"])
-    hours_arr = np.array(measurements[idsensor]["hours"])
-
-    for weekday in np.unique(day_arr):
-        value_arr = hours_arr[(motion_arr == 1) & (day_arr == weekday)]
-        for hour in range(24):
-            myvals[weekday,hour] = sum(value_arr[value_arr == hour])
-        myvals[weekday] = myvals[weekday] / float(sum(myvals[weekday])) # normalize per day
-
-    sns.heatmap(myvals)
+    sns.heatmap(myvals[:,:,ids.index(idsensor)], cmap='Greens')
     plt.xticks(np.array(range(24))+0.5, rotation = 0)
     plt.yticks(np.array(range(len(daynames)))+0.5, daynames, rotation=0)
+
+plt.show()
+system.exit(0)
 
 ##### GRAPHS OF ALL SENSORS
 fig = plt.figure()
